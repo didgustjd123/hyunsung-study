66830d624bb6eefb46a730fa231d37de2bb92a2e
didgustjd123 <34880920+didgustjd123@users.noreply.github.com>
2017-12-27 16:25:11 +0900

    아두이노 코드를 사용한 led 번갈아 깜빡이기

---
+++ blink.ino
@@ -0,0 +1,134 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13,HIGH);
+  delay(500);
+  digitalWrite(13,LOW);
+  digitalWrite(12,HIGH);
+  delay(500);
+  digitalWrite(12,LOW);
+  digitalWrite(13,HIGH);
+
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
